8a7684d3ff58af836042b293d1924f608a37ec3b
Jaiganesh Kumaran <jaiganesh.kumaran@outlook.com>
2021-05-04 18:12:47 +0530

    Added Mvvm framework for C++

---
+++ JuvMvvmCpp/ObservableBase.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "ObservableBase.g.h"
+
+namespace winrt::OneToolkit::Mvvm
+{
+    namespace implementation
+    {
+        struct ObservableBase : ObservableBaseT<ObservableBase>
+        {
+        public:
+            virtual bool Decide(hstring const& propertyName);
+            virtual void WhenPropertyChanged(Windows::UI::Xaml::Data::PropertyChangedEventArgs const& args);
+            void Raise(hstring const& propertyName);
+            event_token PropertyChanged(Windows::UI::Xaml::Data::PropertyChangedEventHandler const& handler);
+            void PropertyChanged(event_token token) noexcept;
+        private:
+            event<Windows::UI::Xaml::Data::PropertyChangedEventHandler> m_PropertyChanged;
+        };
+    }
+    
+    namespace factory_implementation
+    {
+        struct ObservableBase : ObservableBaseT<ObservableBase, implementation::ObservableBase>
+        {
+        };
+    }
+}
