439b3abc40f71d3f02cc64bd8e1000bb16cebf57
Jaiganesh Kumaran <jaiganesh.kumaran@outlook.com>
2021-09-14 16:26:40 +0530

    Implemented TabSwitcher

---
+++ OneToolkit.Dotnet/UI/Input/TabSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneToolkit.UI.Input
+{
+	public static class TabSwitcher
+	{
+		public static ulong GetNewSelectionIndex(ulong currentIndex, ulong collectionSize, bool reverse)
+		{
+			if (collectionSize <= 1) return currentIndex;
+			else if (reverse) return currentIndex == 0 ? collectionSize - 1 : currentIndex - 1;
+			else return currentIndex == collectionSize - 1 ? 0 : currentIndex + 1;
+		}
+	}
+}
